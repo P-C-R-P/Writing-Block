bf38da547026c40d165884d7a542a7fdc0881458
Philippa Payne <philippacrpayne@yahoo.com>
2023-07-29 00:14:55 +0200

    client version 0.3: setting up basic css for home component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 
 import Home from './Home';
+import Flashcards from './Flashcards';
+import Prompts from './Prompts';
+import Saved from './Saved';
+import Submitted from './Submitted';
 
 function App() {
   return (
     <>
-      <Home />
+      <Flashcards />
     </>
   );
 }

1cff4a19cf42e8fa69ccca071c388b9fad1d637d
Philippa Payne <philippacrpayne@yahoo.com>
2023-08-02 11:33:56 +0200

    server update: remove api key

--- server/apis/openai.js
+++ server/apis/openai.js
@@ -1,4 +1,4 @@
-const API_KEY = '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';
+const API_KEY = process.env.API_KEY;
 const API_URL = 'https://api.openai.com/v1/completions';
 
 async function fetchSentence() {
